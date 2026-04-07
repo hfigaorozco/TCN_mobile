0ac3c8a5cfa9eda0cf29d6e9e393befbe980ea8d
GaliDaVinci <leonardovaldez583@gmail.com>
2026-04-06 17:38:05 -0700

    > Quitar importaciones sin usar

--- lib/screens/cambiarcontra_screen.dart
+++ lib/screens/cambiarcontra_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'shared_appbar.dart';
-import 'shared_navbar.dart';
 
 class CambiarContraScreen extends StatefulWidget {
   const CambiarContraScreen({super.key});
