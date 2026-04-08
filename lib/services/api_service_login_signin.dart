d2b46973f764db291307b3a2d3d4069d166f8dd4
GaliDaVinci <leonardovaldez583@gmail.com>
2026-04-08 08:52:02 -0700

    > Servicios de api para login y registro

--- lib/services/api_service_login_signin.dart
+++ lib/services/api_service_login_signin.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/usuario.dart';
 import '../models/login_respuesta.dart';
@@ -20,5 +20,63 @@ class ApiServiceLoginSignin {
     return prefs.getString('auth_token');
   }
 
-  Future<String> quitarToken()
+  //Eliminar el token para logout
+  Future<void> quitarToken() async{
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('auth_token');
+  }
+
+  //Obtener headers con token
+  Future<Map<String, String>> _obtenerHeaders() async{
+    final token = await obtenerToken();
+    return {
+      'content-type': 'application/json',
+      'Authorization': 'Token $token',
+    };
+  }
+
+
+  //Registro
+  Future<Usuario> registrar(String nombre, String email, String password,) async{
+    final url = Uri.parse('$baseUrl/registro/');
+
+    final respuesta = await http.post(
+      url,
+      headers:{'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'nombre': nombre,
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    if (respuesta.statusCode == 201) {
+      return Usuario.fromJson(jsonDecode(respuesta.body));
+    } else {
+      throw Exception(' Error al registrar: ${respuesta.body}');
+    }
+  }
+
+
+  //Login
+  Future<LoginRespuesta> login(String email, String password) async{
+    final url = Uri.parse('$baseUrl/login/');
+    final respuesta = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+      })
+    );
+
+    if(respuesta.statusCode == 200){
+      final data = jsonDecode(respuesta.body);
+      final loginRespuesta = LoginRespuesta.fromJson(data);
+      await guardarToken(loginRespuesta.token);
+      return loginRespuesta;
+    } else {
+      throw Exception('Error al iniciar sesion: ${respuesta.body}');
+    }
+  }
 }
