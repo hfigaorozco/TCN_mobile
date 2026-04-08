c89742524fe9b229e2f858d7c555c4a6e179042f
GaliDaVinci <leonardovaldez583@gmail.com>
2026-04-08 07:39:15 -0700

    > Servicios de api

--- lib/services/api_service_login_signin.dart
+++ lib/services/api_service_login_signin.dart
@@ -3,8 +3,22 @@ import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/usuario.dart';
 import '../models/login_respuesta.dart';
+import '../config.dart';
 
+class ApiServiceLoginSignin {
+  static const String baseUrl = Config.baseUrl;
 
-class ApiServiceLoginSignin {	189.223.49.38
-  static const String baseUrl = 'http://10.0.2.2:8000/transportes-cuervo-negro/api';
+  //Guardar el token de Login
+  Future<void> guardarToken(String token) async{
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('auth_token', token);
+  }
+
+  //Obtener el token guardado
+  Future<String?> obtenerToken() async{
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('auth_token');
+  }
+
+  Future<String> quitarToken()
 }
