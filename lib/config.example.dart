8b6ecb5af8841bbc5aadd26997343fb6597ac55c
GaliDaVinci <leonardovaldez583@gmail.com>
2026-04-08 07:10:45 -0700

    > Archivo de configuracion ejemplo

---
+++ lib/config.example.dart
@@ -0,0 +1,8 @@
+// 1. Crear un archivo llamado 'config.dart'
+// 2. Copiar el codigo de este archivo
+// 3. Editar la variable baseUrl y poner tu IP
+
+class Config {
+  static const String baseUrl =
+      'http://TU_IP:8000/transportes-cuervo-negro/api';
+}
---
+++ lib/services/api_service_login_signin.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../models/usuario.dart';
+import '../models/login_respuesta.dart';
+
+
+class ApiServiceLoginSignin {	189.223.49.38
+  static const String baseUrl = 'http://10.0.2.2:8000/transportes-cuervo-negro/api';
+}
