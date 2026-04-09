cf4e63c546ccc370965198365895c28f7c423912
Hector <hfigaorozco@gmail.com>
2026-04-08 22:07:43 -0700

    Inicio de sesion y flujo de compra del boleto fusionados

--- lib/models/login_respuesta.dart
+++ lib/models/login_respuesta.dart
@@ -2,24 +2,21 @@ class LoginRespuesta {
   final String token;
   final int id;
   final String email;
-  final String password;
   final String nombre;
 
   LoginRespuesta({
     required this.token,
     required this.id,
     required this.email,
-    required this.password,
     required this.nombre,
   });
 
   factory LoginRespuesta.fromJson(Map<String, dynamic> json) {
     return LoginRespuesta(
-      token: json['token'],
-      id: json['id'],
-      email: json['email'],
-      password: json['password'],
-      nombre: json['nombre'],
+      token: json['token'] ?? '',
+      id: json['user_id'] ?? 0,
+      email: json['email'] ?? '',
+      nombre: json['nombre'] ?? '',
     );
   }
 }
